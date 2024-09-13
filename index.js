3ce562fc9d24f242a9bdcf9fc10bb576b7707137
NIcholas Souza <nicholas.souza@filadelfia.br>
2024-09-13 16:17:04 -0300

    quebra da linha

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function foto(idModal) {
     var modal = document.getElementById(idModal);
-    
+ 
     if (modal.style.display == "block") {
         modal.style.display = "none";    
     }else{
         modal.style.display = "block";    
     }
+
+  
 
 }
